96549addca1e1b094d875ee27ec6eb9f50b5fd26
Kamila Ćwikła <kamila.cwikla8@gmail.com>
2026-01-12 00:31:23 +0100

    add comments to poisson newton eq with spe data

--- FEniCS/eq_newton_spe/oil_extr_eq_newton_spe.py
+++ FEniCS/eq_newton_spe/oil_extr_eq_newton_spe.py
@@ -9,46 +9,52 @@ from dolfinx.io import XDMFFile
 import csv
 from scipy.interpolate import RegularGridInterpolator
 
-#dane SPE
+#dane SPE model 1, więcej info w spe_data.ipynb
 REAL_NX, REAL_NY = 100, 20
 REAL_LX, REAL_LY = 762.0, 15.24
 
 #skalowanie danych do zakresu [1, 10] 
 def load_and_scale_kq(path):
-    raw_data = np.loadtxt(path).flatten()[:2000]
+    raw_data = np.loadtxt(path).flatten()[:2000] #zamieniay na długi wektor i bierzemy 200 pierwszych elementów bo 100*20 to 2000
     k_min, k_max = raw_data.min(), raw_data.max()
     k_scaled = (raw_data - k_min) / (k_max - k_min) * (10.0 - 1.0) + 1.0
-    return k_scaled.reshape((REAL_NY, REAL_NX))
+    return k_scaled.reshape((REAL_NY, REAL_NX)) #wracamy do kształtu macierzy
 
 kq_data_matrix = load_and_scale_kq(r'/mnt/c/PINN_mgr/perm_case1.dat')
 
 #interpolator
 x_coords = np.linspace(0, REAL_LX, REAL_NX)
 y_coords = np.linspace(0, REAL_LY, REAL_NY)
+
+#ciagła przestrzeń geometryczna do symulacji, wartości poza danymi przyjmują wartość 1.0 -> bounds_error=False, fill_value=1.0
 interp_kq = RegularGridInterpolator((y_coords, x_coords), kq_data_matrix, 
                                     bounds_error=False, fill_value=1.0)
 
 
+#tworzenie katalogów na wyniki
 Path("results/fields").mkdir(parents=True, exist_ok=True)
 Path("results/metrics").mkdir(parents=True, exist_ok=True)
 
-# nx, ny = 8, 8
-# domain = mesh.create_unit_square(MPI.COMM_WORLD, nx, ny)
+#MPI.COMM_WORLD to komunikator dla wszystkich procesów
+#tworze prostokąt o początku w 0,0 i końcu w wymiarach podanych w SPE, calosć dziele na 100x20 elementów
 domain = mesh.create_rectangle(MPI.COMM_WORLD, 
                                [np.array([0, 0]), np.array([REAL_LX, REAL_LY])], 
                                [REAL_NX, REAL_NY])
 
+
+#przestrzeń funkcyjna Lagrangea gdzie stopień wielomianu to 1
 V = fem.functionspace(domain, ("Lagrange", 1))
 
 Kq = fem.Function(V)
 def kq_map(x):
     # Mapowanie punktów siatki MES na wartości z interpolatora
-    pts = np.stack((x[1], x[0]), axis=-1)
-    return interp_kq(pts)
+    pts = np.stack((x[1], x[0]), axis=-1) #zamieniony x z y, odwrotnie do tego jak zdefiniowaliśmy interpolator
+    return interp_kq(pts) #interpolator oblicza wartość przepuszczalności w tych punktach
 
-Kq.interpolate(kq_map)
+Kq.interpolate(kq_map) #wypełniamy siatkę wartościami przepuszczalności
 
 u_n = fem.Function(V)
+#początkowe warunki u(x,0), jest w kółku daj ciśnienie 1 a jeśli nie to 0
 u_n.interpolate(lambda x: np.where(np.sqrt((x[0]-REAL_LX/2)**2 + (x[1]-REAL_LY/2)**2) <= 2.0, 1.0, 0.0))
 
 h = fem.Function(V)
@@ -59,55 +65,63 @@ dt = 0.001
 T = 2.0
 num_steps = int(T/dt)
 
+#nienane rozwiązanie
 uh = fem.Function(V)
+
+#funkcja testowa do słabego sformułowania
 v = ufl.TestFunction(V)
 
+#ze wzoru
 def K(u):
     return Kq * ufl.exp(10 * u)
 
+#słabe równanie
 F = (
-    (uh - u_n)/dt * v * ufl.dx
+    (uh - u_n)/dt * v * ufl.dx #dyskretyzacja w czasie Eulera w przód
     + ufl.inner(K(uh) * ufl.grad(uh), ufl.grad(v)) * ufl.dx
     - h * v * ufl.dx
 )
 
-fdim = domain.topology.dim - 1
-domain.topology.create_connectivity(domain.topology.dim - 1, domain.topology.dim)
-boundary_facets = mesh.exterior_facet_indices(domain.topology)
-bc = fem.dirichletbc(PETSc.ScalarType(0), fem.locate_dofs_topological(V, fdim, boundary_facets), V)
+fdim = domain.topology.dim - 1 #wymiar krawędzi
+domain.topology.create_connectivity(domain.topology.dim - 1, domain.topology.dim) #związe między wnętrzem a krawędziami
+boundary_facets = mesh.exterior_facet_indices(domain.topology) #indeksy zewnętrznych krawędzi
+bc = fem.dirichletbc(PETSc.ScalarType(0), fem.locate_dofs_topological(V, fdim, boundary_facets), V) #Dirichlet BC u=0 na całej granicy
 
 petsc_options = {
-    "snes_type": "newtonls",
-    "snes_linesearch_type": "bt", 
-    "snes_rtol": 1e-6,
-    "snes_atol": 1e-6,
-    "ksp_type": "gmres",
-    "pc_type": "hypre",
-    "pc_hypre_type": "boomeramg",
+    "snes_type": "newtonls", #metoda Netwona
+    "snes_linesearch_type": "bt", #jeśli krok newtona zbyt duży to się wycofa
+    "snes_rtol": 1e-6, #solver kończy pracę, gdy błąd względny spadnie o 6 rzędów wielkości.
+    "snes_atol": 1e-6, #lub gdy błąd bezwzględny spadnie poniżej tej wartości.
+    "ksp_type": "gmres", #Generalized Minimal Residual method
+    "pc_type": "hypre", 
+    "pc_hypre_type": "boomeramg", #Algebraic Multigrid (AMG)
 }
+
 
 problem = NonlinearProblem(F, uh, bcs=[bc], petsc_options=petsc_options, petsc_options_prefix="TimeStep")
 
 
-xdmf_file = XDMFFile(MPI.COMM_WORLD, "results/fields/solution.xdmf", "w")
-xdmf_file.write_mesh(domain)
+xdmf_file = XDMFFile(MPI.COMM_WORLD, "results/fields/solution.xdmf", "w") #dane wizualne
+xdmf_file.write_mesh(domain) 
 
 metrics_file = open("results/metrics/metrics.csv", "w", newline="")
 csv_writer = csv.writer(metrics_file)
 csv_writer.writerow(["time", "delta_L2", "max_u", "mean_u"])
 
 for n in range(num_steps):
-    t = (n + 1) * dt
-    problem.solve()
-    snes = problem.solver
-    num_its = snes.getIterationNumber()
+    t = (n + 1) * dt #aktualny czas
+    problem.solve() #rozwiązanie równania nieliniowego
+    snes = problem.solver #dostep do solvera
+    num_its = snes.getIterationNumber() #liczba iteracji newtona
 
     uh.name = "Pressure"
-    xdmf_file.write_function(uh, t)
+    xdmf_file.write_function(uh, t) #aktualne pole ciśnienia
     
     #metryki
-    dx = ufl.dx(domain=domain)
+    dx = ufl.dx(domain=domain) #miara całkowania
 
+    #allReduce sumuje ze wszystkich procesorów
+    #całka z 1.0 po całej domenie to objętość
     vol = domain.comm.allreduce(
         fem.assemble_scalar(fem.form(1.0 * dx)), op=MPI.SUM
     )
@@ -130,26 +144,26 @@ for n in range(num_steps):
         csv_writer.writerow([t, delta_L2, max_u, mean_u]) 
         print(f"Step {n+1}/{num_steps}, t={t:.3f}, max_u: {max_u:.4e}, iterations: {num_its}")
     
+    #aktualizacja rozwiązania na kolejny krok czasowy
     u_n.x.array[:] = uh.x.array
 
 xdmf_file.close()
 metrics_file.close()
 
 
+# DANE DO PORÓWNANIA Z PINN 
 def get_values_at_points(points, function):
     from dolfinx import geometry
 
-    # points: (N, 2) → (N, 3)
+    #konwertujemy punkty 2D na 3D, ustawiając z=0
     points_3d = np.zeros((points.shape[0], 3), dtype=np.float64)
     points_3d[:, :2] = points
 
-    # 1. Bounding box tree – UWAGA: przekazujemy DOMAIN, nie topology
+    #pozwala szybko sprawdzić, w której części siatki szukać punktu, zamiast przeszukiwać każdą komórkę po kolei
     bb_tree = geometry.bb_tree(domain, domain.topology.dim)
 
-    # 2. Szukamy kandydatów komórek
     cell_candidates = geometry.compute_collisions_points(bb_tree, points_3d)
 
-    # 3. Sprawdzamy faktyczne kolizje
     colliding_cells = geometry.compute_colliding_cells(domain, cell_candidates, points_3d)
 
     values = np.full(points.shape[0], np.nan)
@@ -157,6 +171,7 @@ def get_values_at_points(points, function):
     for i in range(points.shape[0]):
         cells = colliding_cells.links(i)
         if len(cells) > 0:
+            #jeśli punkt jest w siatce, pobierz wartość funkcji w tym punkcie
             values[i] = function.eval(points_3d[i:i+1], np.array([cells[0]], dtype=np.int32))[0]
 
     return values
@@ -166,15 +181,14 @@ def get_values_at_points(points, function):
 x_test = np.linspace(0, REAL_LX, 100)
 y_test = np.linspace(0, REAL_LY, 100)
 X, Y = np.meshgrid(x_test, y_test)
-pts_to_eval = np.vstack((X.flatten(), Y.flatten())).T
+pts_to_eval = np.vstack((X.flatten(), Y.flatten())).T #punkty w formacie 10000x2 
 
-# 2. Pobieramy wartości końcowego rozwiązania uh
+#pobieramy wartości końcowego rozwiązania uh
 if MPI.COMM_WORLD.rank == 0:
     u_fenics_values = get_values_at_points(pts_to_eval, uh)
 else:
     u_fenics_values = None
 
-# 3. Zapisujemy do pliku, który łatwo wczytasz w Pythonie/PINN
 if MPI.COMM_WORLD.rank == 0:
     comparison_data = {
         "x": X,
