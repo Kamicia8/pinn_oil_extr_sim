894605b2971168e4ac33cc90e3fa9418290bd256
Kamila Ćwikła <kamila.cwikla8@gmail.com>
2026-06-13 10:03:14 +0200

    delete pinnhp

--- pinn_hp/src/adaptations/adaptations_1D/__init__.py
+++
@@ -1,9 +0,0 @@
-from .density_sampling import DensitySamplingAdaptation1D
-from .middle_point import MiddlePointAdaptation1D
-from .no_adaptation import NoAdaptation1D
-
-__all__ = [
-    "DensitySamplingAdaptation1D",
-    "MiddlePointAdaptation1D",
-    "NoAdaptation1D",
-]
--- pinn_hp/src/adaptations/adaptations_2D/__init__.py
+++
@@ -1,7 +0,0 @@
-from .density_sampling import DensitySamplingAdaptation2D
-from .no_adaptation import NoAdaptation2D
-
-__all__ = [
-    "DensitySamplingAdaptation2D",
-    "NoAdaptation2D",
-]
